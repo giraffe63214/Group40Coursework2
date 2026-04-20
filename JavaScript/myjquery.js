b1a3931228aed236f1ba1e1062ae495e68c2e2d8
giraffe63214 <gspratt487@outlook.com>
2026-04-20 16:11:24 +0000

    finished About
    added image transitions to about page

--- JavaScript/myjquery.js
+++ JavaScript/myjquery.js
@@ -1,5 +1,5 @@
  $(document).ready(function(){
-  setInterval(swapImages,3000);
+  setInterval(swapImages,4000);
   // hides contact information
   $("#invis1").hide();
    // controls the display of contact information using the contact button
@@ -14,9 +14,37 @@
 	  $("#contactButton").text("Show Contact Info");
 	}
   });
+  // cycles through the images on the About Page
   function swapImages(){
-	  $("#ourCoffee").hide();
-	  document.getElementById("ourCoffee").src = "Images/About_item/coffee2.jpg";
-	  $("#ourCoffee").fadeIn(2000);
+	  let x = $("#ourCoffee").attr("src");
+      $("#ourCoffee").hide();
+	  $("#ourStore").hide();
+	  if (x == "Images/About_item/coffee1.jpg"){
+		   $("#ourCoffee").attr("src","Images/About_item/coffee2.jpg");
+		   $("#ourStore").attr("src","Images/About_item/store2.jpg");
+		   x = $("#ourCoffee").attr("src");
+		   $("#ourCoffee").fadeIn(500);
+		   $("#ourStore").fadeIn(500);
+		   
+	  }
+	  else if (x == "Images/About_item/coffee2.jpg"){
+		   $("#ourCoffee").attr("src","Images/About_item/coffee3.jpg");
+		   $("#ourStore").attr("src","Images/About_item/store3.jpg");
+		   x = $("#ourCoffee").attr("src");
+		   $("#ourCoffee").fadeIn(500);
+		   $("#ourStore").fadeIn(500);
+	  }
+	  else if (x == "Images/About_item/coffee3.jpg"){
+		   $("#ourCoffee").attr("src","Images/About_item/coffee1.jpg");
+		   $("#ourStore").attr("src","Images/About_item/store1.jpg");
+		   x = $("#ourCoffee").attr("src");
+		   $("#ourCoffee").fadeIn(500);
+		   $("#ourStore").fadeIn(500);
+	  }
+	    
   }
+  
+  
+  
+ 
 });
